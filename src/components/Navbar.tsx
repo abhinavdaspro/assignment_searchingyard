ce0b54cb0378a6cc31c29c8a02b88cf61631c0e6
Abhinav Das <abhinavdaspro@gmail.com>
2023-03-29 21:58:16 +0530

    change in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 export const Navbar = () => {
   return (
     <div className="w-full bg-white h-16 flex items-center px-4 sm:px-8 md:px-20 shadow-nav sticky top-0 z-10">
+     <Link href="/">
       <p className="font-mont text-xl font-normal uppercase text-primary">
         mock crud app
       </p>
+      </Link>
     </div>
   );
 };
