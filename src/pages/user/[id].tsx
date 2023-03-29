a48c9d26d3777910a50347266bae7035c0ffae8c
Abhinav Das <abhinavdaspro@gmail.com>
2023-03-29 19:16:36 +0530

    remove user function change

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -16,7 +16,7 @@ const UserOverView = () => {
         <div className="flex items-center pb-4">
           <FaUserCircle className="h-8 w-8 text-slate-600" />
           <p className="pl-3 text-primary font-poppins text-xl">
-            {singleUser.name}
+            {singleUser?.name}
           </p>
           <button className="flex items-center ml-auto text-primary bg-white hover:bg-primary hover:text-white hover:shadow-lg hover:-translate-y-1 font-poppins text-base border border-primary px-2 py-1 rounded-md transition-all duration-200 ease-in-out">
             <FaUserEdit className="h-4 w-4 mr-3" />
@@ -29,25 +29,25 @@ const UserOverView = () => {
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Username:</p>
               <p className="text-base font-poppins ml-2">
-                {singleUser.username}
+                {singleUser?.username}
               </p>
             </div>
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Email:</p>
-              <p className="text-base font-poppins ml-2">{singleUser.email}</p>
+              <p className="text-base font-poppins ml-2">{singleUser?.email}</p>
             </div>
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Phone:</p>
-              <p className="text-base font-poppins ml-2">{singleUser.phone}</p>
+              <p className="text-base font-poppins ml-2">{singleUser?.phone}</p>
             </div>
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Phone:</p>
-              <p className="text-base font-poppins ml-2">{singleUser.phone}</p>
+              <p className="text-base font-poppins ml-2">{singleUser?.phone}</p>
             </div>
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Web:</p>
               <p className="text-base font-poppins ml-2">
-                {singleUser.website}
+                {singleUser?.website}
               </p>
             </div>
           </div>
@@ -55,25 +55,25 @@ const UserOverView = () => {
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Company:</p>
               <p className="text-base font-poppins ml-2">
-                {singleUser.company.name}
+                {singleUser?.company?.name}
               </p>
             </div>
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Address:</p>
               <p className="text-base font-poppins ml-2">
-                {`${singleUser.address.street}, ${singleUser.address.suite}`}
+                {`${singleUser?.address?.street}, ${singleUser?.address?.suite}`}
               </p>
             </div>
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">City:</p>
               <p className="text-base font-poppins ml-2">
-                {singleUser.address.city}
+                {singleUser?.address?.city}
               </p>
             </div>
             <div className="flex items-center">
               <p className="text-base font-poppins text-slate-500">Zip Code:</p>
               <p className="text-base font-poppins ml-2">
-                {singleUser.address.zipcode}
+                {singleUser?.address?.zipcode}
               </p>
             </div>
           </div>
