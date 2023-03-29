95f41bc540b07986a595c59a6f2a3a37ef3a566f
Abhinav Das <abhinavdaspro@gmail.com>
2023-03-29 21:56:23 +0530

    404 page change

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,13 @@
+import { Header } from "@/components/Header";
 import React from "react";
 
 const ErrorPage= ()=>{
-    return <div  className="w-full text-center ">
+    return  <>
+    <Header pageName={'Not Found'}/>
+    <div  className="w-full text-center ">
             <p className="text-primary font-mont font-semibold">Sorry, The page is not available.</p>
     </div>
+    </>
 }
 
 
