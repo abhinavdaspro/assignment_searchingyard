d4bc96faefa59eb9ecb1785029bedbb9e06b2f3a
Abhinav Das <abhinavdaspro@gmail.com>
2023-03-29 21:51:16 +0530

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage= ()=>{
+    return <div  className="w-screen text-center ">
+            <p className="text-primary font-mont font-semibold">Sorry, The page is not available.</p>
+    </div>
+}
+
+
+export default ErrorPage
+
