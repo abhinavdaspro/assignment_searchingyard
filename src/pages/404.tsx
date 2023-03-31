2df0a52b1940a06c038787618ae6836974f35b7d
Abhinav Das <abhinavdaspro@gmail.com>
2023-03-31 13:33:33 +0100

    font change

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,17 @@
 import { Header } from "@/components/Header";
 import React from "react";
 
-const ErrorPage= ()=>{
-    return  <>
-    <Header pageName={'Not Found'}/>
-    <div  className="w-full text-center ">
-            <p className="text-primary font-mont font-semibold">Sorry, The page is not available.</p>
-    </div>
+const ErrorPage = () => {
+  return (
+    <>
+      <Header pageName={"Not Found"} />
+      <div className="w-full text-center ">
+        <p className="text-primary font-mont font-light">
+          Sorry, The page is not available.
+        </p>
+      </div>
     </>
-}
+  );
+};
 
-
-export default ErrorPage
-
+export default ErrorPage;
